bed5d3e12b097916bdf82af5d76af0dc06c166ad
Dmitry <dmitry.emelianenko@lyte.com>
2024-07-04 18:10:27 +0400

    Test migrations

--- FinTech/settings.py
+++ FinTech/settings.py
@@ -55,8 +55,13 @@ WSGI_APPLICATION = "FinTech.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "fintech",
+        "USER": "fintech",
+        "PASSWORD": "fintech",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+        "CONN_MAX_AGE": None,
     }
 }
 
